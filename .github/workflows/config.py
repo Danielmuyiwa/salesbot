cafe7f3698d0990a444772eab470e5649164dbeb
Danielmuyiwa <41693468+Danielmuyiwa@users.noreply.github.com>
2026-03-03 19:27:28 +0100

    Create config.py

---
+++ .github/workflows/config.py
@@ -0,0 +1,7 @@
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+DEXSCREENER_URL = "https://api.dexscreener.com/latest/dex/pairs"
